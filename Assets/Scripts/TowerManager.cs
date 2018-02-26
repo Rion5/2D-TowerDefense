3d055da3ba9b5aef3b2d272ed817c42615b96916
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-02-25 20:49:14 -0500

    Added RayCast
    Ray cast so we can get position of mouse click position.

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class TowerManager : Singleton<TowerManager> {
     private TowerButton towerButtonPressed;
-
-    public void selectedTower(TowerButton towerSelected)
-    {
-        towerButtonPressed = towerSelected;
-        Debug.Log("Pressed: " + towerButtonPressed.gameObject);
-    }
 	// Use this for initialization
 	void Start () {
 		
@@ -17,6 +11,34 @@ public class TowerManager : Singleton<TowerManager> {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0))
+        {
+            //worldPoint is the position of the mouse click.
+            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
+            /* Ray Cast involves intersecting a ray with the object in an environment.
+             * The ray cast tells you what objects in the environment the ray runs into.
+             * and may return additional information as well, such as intersection point
+             */
+            //Finding the worldPoint of where we click, from Vector2.zero (which is buttom left corner)
+            RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+            placeTower(hit);
+        }
 	}
+
+   
+    public void selectedTower(TowerButton towerSelected)
+    {
+        towerButtonPressed = towerSelected;
+        //Debug.Log("Pressed: " + towerButtonPressed.gameObject);
+    }
+
+    //Place new tower on the mouse click location
+    public void placeTower(RaycastHit2D hit)
+    {
+        GameObject newTower = Instantiate(towerButtonPressed.TowerObject);
+        newTower.transform.position = hit.transform.position;
+    }
 }
